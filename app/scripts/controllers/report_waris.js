b17defa9229366259393bb585e21b79e152a7081
Maitha Manyala <maitha.manyala@gmail.com>
2015-07-26 20:53:20 +0300

    Implemented waris report

---
+++ app/scripts/controllers/report_waris.js
@@ -0,0 +1,90 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name majiApp.controller:BillingSummaryCtrl
+ * @description
+ * # BillingSummaryCtrl
+ * Controller of the majiApp
+ */
+  app.controller('WarisCtrl', function ($scope, $http, appService, $cookieStore, $state, $mdDialog, $mdToast, $animate, $rootScope,$stateParams) {
+    var config = appService.getCofig();
+    $scope.progress = false;
+    $scope.report = false;
+    $scope.form = {};
+
+    $scope.status =
+      [
+        {'name': 'All'},
+        {'name': 'Active'},
+        {'name': 'Inactive'}
+      ];
+
+    var request = {};
+    request.page = 0;
+    request.size = 100;
+
+    //Load zones
+    appService.getZones(request).success(function (response) {
+      $scope.zones = response.payload.content;
+    }).error(function (data, status) {
+      $state.go('session');
+    });
+
+    appService.getAllBillingMonths(request).success(function (response) {
+      console.log(response);
+      $scope.billingMonths = response.payload;
+    }).error(function (data, status) {
+      $state.go('session');
+    });
+
+
+    $scope.generate = function (form) {
+      $scope.progress = true;
+      $scope.report = false;
+      //Get active billing month
+      var request = {};
+
+      var myForm = $scope.myForm.object;
+
+      //select zone
+      var billingMonth = form.billingMonth;
+      if (typeof billingMonth !== 'undefined') {
+        request.billingMonthId = $scope.billingMonths[billingMonth].billingMonthId;
+      }
+
+      //select zone
+      var zone = form.accZone;
+      if (typeof zone !== 'undefined') {
+        request.zoneId = $scope.zones[zone].zoneId;
+      }
+
+      var accountStatus = form.status;
+      if (typeof accountStatus !== 'undefined') {
+        request.accountStatus = $scope.status[accountStatus].name;
+      }
+
+      var params= {};
+      params.fields = request;
+
+      appService.getWarisReport(params).success(function (response) {
+        $scope.progress = false;
+        $scope.error = false;
+        $scope.data = response.payload;
+        $scope.records = response.payload.content;
+        $scope.report = true;
+
+      }).error(function (data, status) {
+        if (status === 401) {
+          $scope.progress = false;
+          $state.go('session');
+          $scope.message = data.message;
+        } else {
+          $scope.progress = false;
+          $scope.error = true;
+          $scope.message = data.message;
+        }
+      });
+
+    };
+  });
--- app/scripts/services/appservice.js
+++ app/scripts/services/appservice.js
@@ -822,6 +822,17 @@ app.service('appService', function ($http, $cookieStore) {
     });
   };
 
+  this.getWarisReport = function (request) {
+    return $http({
+      method: POST_REQUEST,
+      url: BASE_URL + REPORTS + '/waris',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: angular.toJson(getPayload(request))
+    });
+  };
+
   this.getAccountStatementReport = function (request, accountId) {
     return $http({
       method: POST_REQUEST,
